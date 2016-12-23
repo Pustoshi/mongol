8098401f7052436db64522720b291a13d4c115d0
pustoshi <pustoshi@gmail.com>
2016-12-23 03:43:03 +0300

    calculate

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  
+
   $('.owl-carousel').owlCarousel({
       loop:true,
       margin:10,
@@ -18,24 +18,13 @@ $(document).ready(function() {
       }
   })
 
-  $('.smoothScroll').click(function(event) {
-    event.preventDefault();
-    var href=$(this).attr('href');
-    var target=$(href);
-    var top=target.offset().top;
-    $('html,body').animate({
-      scrollTop: top
-    }, 1000);
-    });
-
-  $(function () {
-    var austDay = new Date();                               // сегодняшняя дата
-    austDay = new Date(austDay.getFullYear() + 1, 0, 1);    // дата, до которой считаем
-    // console.log(austDay);
-    $('#countdown').countdown({until: austDay, padZeroes: true});
+  $('#clock').countdown('2020/10/10', function(event) {
+    var $this = $(this).html(event.strftime(''
+      + '<span>%w</span> weeks '
+      + '<span>%d</span> days '
+      + '<span>%H</span> hr '
+      + '<span>%M</span> min '
+      + '<span>%S</span> sec'));
   });
-
-  var scene =document.getElementById('fly__scene')
-  var parallax = new Parallax(scene);
 
 });
